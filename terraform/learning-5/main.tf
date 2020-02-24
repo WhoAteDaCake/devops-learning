38cccecbc43e3f4ee5b8c369f3c97d2656cfed9d
WhoAteDaCake <doitsimplefy@gmail.com>
2020-02-24 18:08:38 +0000

    (feat) remove subdomain zone

--- terraform/learning-5/main.tf
+++ terraform/learning-5/main.tf
@@ -23,23 +23,58 @@ resource "google_dns_managed_zone" "root" {
   depends_on = [module.project_services]
 }
 
-# Name server
-resource "google_dns_record_set" "ns" {
-  name         = google_dns_managed_zone.root.dns_name
-  managed_zone = google_dns_managed_zone.root.name
-  type         = "NS"
-  ttl          = 60
 
-  rrdatas = [
-    "ns-cloud-a1.googledomains.com.",
-    "ns-cloud-a2.googledomains.com.",
-    "ns-cloud-a3.googledomains.com.",
-    "ns-cloud-a4.googledomains.com."
-  ]
-}
+# resource "google_dns_managed_zone" "subdmn" {
+#   name     = "test-su-domain"
+#   visibility = "public"
+#   description = "Test domain"
+
+#   dns_name = "test.${var.domain}."
+#   depends_on = [module.project_services]
+# }
+
+
+# # Name server
+# resource "google_dns_record_set" "ns" {
+#   name         = google_dns_managed_zone.root.dns_name
+#   managed_zone = google_dns_managed_zone.root.name
+#   type         = "NS"
+#   ttl          = 60
+
+#   rrdatas = [
+#     "ns-cloud-a1.googledomains.com.",
+#     "ns-cloud-a2.googledomains.com.",
+#     "ns-cloud-a3.googledomains.com.",
+#     "ns-cloud-a4.googledomains.com."
+#   ]
+# }
+
+# # Name server
+# resource "google_dns_record_set" "ns-sub" {
+#   name         = "test.${google_dns_managed_zone.root.dns_name}"
+#   managed_zone = google_dns_managed_zone.root.name
+#   type         = "NS"
+#   ttl          = 60
+
+#   rrdatas = [
+#     "ns-cloud-a1.googledomains.com.",
+#     "ns-cloud-a2.googledomains.com.",
+#     "ns-cloud-a3.googledomains.com.",
+#     "ns-cloud-a4.googledomains.com."
+#   ]
+# }
 
 resource "google_dns_record_set" "a" {
-  name         = google_dns_managed_zone.root.dns_name
+  name         = "@.${google_dns_managed_zone.root.dns_name}"
+  managed_zone = google_dns_managed_zone.root.name
+  type         = "A"
+  ttl          = 60
+
+  rrdatas = [var.ip_address]
+}
+
+resource "google_dns_record_set" "a-test-sub" {
+  name         = "test.${google_dns_managed_zone.root.dns_name}"
   managed_zone = google_dns_managed_zone.root.name
   type         = "A"
   ttl          = 60
